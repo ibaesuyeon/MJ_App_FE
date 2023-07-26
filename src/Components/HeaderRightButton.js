cd5d6e1c2f9dc2fbe1ba20b6b34b543e9151d16b
ibaesuyeon <ibaesuyeon@Naver.com>
2023-07-27 04:06:59 +0900

    [Feat] Stack Navigation, Header
    스택 네비게이션, 헤더, 헤더버튼 구현

---
+++ src/Components/HeaderRightButton.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Pressable } from 'react-native';
+import Icon_Ft from 'react-native-vector-icons/Feather';
+import { BLACK, GRAY } from '../Colors';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+const HeaderRightButton = () => {
+  //   const navigation = useNavigation();
+
+  // if(!canGoBack) {
+  //     return (
+
+  //     );
+  // }
+  return (
+    <Pressable>
+      <Icon_Ft
+        name="user"
+        color={BLACK}
+        size={30}
+        style={[styles.logo, styles.header_right]}
+        onPress={() => {
+          console.log('HeaderRightButton - user');
+        }}
+      />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 30,
+    height: 30,
+    marginHorizontal: 5,
+  },
+});
+export default HeaderRightButton;
--- src/Navigations/MainStack.js
+++ src/Navigations/MainStack.js
@@ -1,8 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import HeaderLeftButton from '../components/HeaderLeftButton';
-// import HeaderRightButton from '../components/HeaderRightButton';
 import MainScreen from '../Screens/MainScreen';
-import { WHITE } from '../Colors';
+import NormalNoticeScreen from '../Screens/NormalNoticeScreen';
+import CreditScreen from '../Screens/CreditScreen';
+import { BLACK, WHITE } from '../Colors';
+import HeaderLeftButton from '../Components/HeaderLeftButton';
+import HeaderRightButton from '../Components/HeaderRightButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,11 +14,23 @@ const MainStack = () => {
       screenOptions={{
         contentStyle: { backgroundColor: WHITE },
         headerTitleAlign: 'center',
-        headerTitleStyle: { fontWeight: '700' },
+        headerTitleStyle: { fontWeight: '900' },
         headerBackTitleVisible: false,
         headerTitle: 'APP_NAME',
+        headerLeft: () => <HeaderLeftButton />,
+        headerRight: () => <HeaderRightButton />,
       }}
     >
+      <Stack.Screen
+        name={'Credit'}
+        component={CreditScreen}
+        options={{ headerTitle: '이수 학점' }}
+      />
+      <Stack.Screen
+        name={'NormalNotice'}
+        component={NormalNoticeScreen}
+        options={{ headerTitle: '일반 공지' }}
+      />
       <Stack.Screen name={'Main'} component={MainScreen} />
     </Stack.Navigator>
   );
